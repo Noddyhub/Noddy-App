116d45b17aaa5ffef4e2eab4f40cecbdbd2e5404
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-13 16:58:49 +0900

    feat: 앱화로 인한 메인 파일 변경 #18
    * 메인 파일: main.swift -> AppDelegate.swift

--- swiftForNoddy/main.swift
+++ swiftForNoddy/main.swift
@@ -5,82 +5,96 @@ let motionManager = CMHeadphoneMotionManager()
 var isWearingAirPods = true
 var lastMotionTimestamp = Date()
 
-func moveCursor(to point: CGPoint) {
-    let move = CGEvent(mouseEventSource: nil, mouseType: .mouseMoved, mouseCursorPosition: point, mouseButton: .left)
-    move?.post(tap: .cghidEventTap)
-}
+let callbackId = UUID().uuidString
+let loginUrl = "https://www.noddy.co.kr/oauth?callbackId=\(callbackId)"
 
-webSocketTask.resume()
-receiveDecodedData()
-sendPairingMessage()
-
-startKeyEventMonitor()
-moveCursor(to: currentCursorPos)
-motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
-    guard let motion = motion else { return }
-
-    lastMotionTimestamp = Date()
-    isWearingAirPods = true
-
-    let attitude = motion.attitude
-    let pitch = attitude.pitch
-    let yaw = -attitude.yaw
-
-    filteredPitch = filteredPitch + filterAlpha * (pitch - filteredPitch)
-    filteredYaw = filteredYaw + filterAlpha * (yaw - filteredYaw)
-
-    let movementThreshold = 0.0088
-    let finalPitch = abs(filteredPitch) < movementThreshold ? 0 : filteredPitch
-    let finalYaw = abs(filteredYaw) < movementThreshold ? 0 : filteredYaw
-
-    let normalizePi: Double = .pi / cursorSensitivity
-    let normalizedPitch = ((finalPitch) + normalizePi / 2) / normalizePi + pitchOffset
-    let normalizedYaw = ((finalYaw) + normalizePi) / (2 * normalizePi) + yawOffset
-
-    let screenWidth = screenFrame.width
-    let screenHeight = screenFrame.height
-
-    let mappedX = screenWidth * CGFloat(normalizedYaw)
-    let mappedY = screenHeight * (1 - CGFloat(normalizedPitch))
-
-    pitchForScroll = mappedY
-
-    let truncatedPitch = Double(Int(normalizedPitch * 100)) / 100
-    let truncatedYaw = Double(Int(normalizedYaw * 100)) / 100
-
-    sendMotionData(pitch: truncatedPitch, yaw: truncatedYaw, name: airPodsName, macBattery: MacBattery, airpodLeftBattery: airPodsLeftBattery ?? 0, airpodRightBattery: airPodsRightBattery ?? 0)
-
-    targetCursorPos = CGPoint(x: mappedX, y: mappedY)
-}
-
-Timer.scheduledTimer(withTimeInterval: 1.0 / 60.0, repeats: true) { _ in
-    guard !isMotionPaused else { return }
-
-    func lerp(_ a: CGFloat, _ b: CGFloat, t: CGFloat) -> CGFloat {
-        return a + (b - a) * t
+func mainLogic() {
+    if let url = URL(string: loginUrl) {
+        NSWorkspace.shared.open(url)
     }
 
-    currentCursorPos.x = lerp(currentCursorPos.x, targetCursorPos.x, t: 0.2)
-    currentCursorPos.y = lerp(currentCursorPos.y, targetCursorPos.y, t: 0.2)
+    pollForToken(callbackId: callbackId) { token in
+        if let token = token {
+            print("✅ JWT 받음: \(token)")
+            uuid = token
+        } else {
+            print("❌ 토큰을 받지 못함 (타임아웃 또는 오류)")
+        }
+    }
+
+
+    func moveCursor(to point: CGPoint) {
+        let move = CGEvent(mouseEventSource: nil, mouseType: .mouseMoved, mouseCursorPosition: point, mouseButton: .left)
+        move?.post(tap: .cghidEventTap)
+    }
+
+
+    webSocketTask.resume()
+    receiveDecodedData()
+    sendPairingMessage()
+
+    startKeyEventMonitor()
     moveCursor(to: currentCursorPos)
+    motionManager.startDeviceMotionUpdates(to: .main) { motion, error in
+        guard let motion = motion else { return }
+
+        lastMotionTimestamp = Date()
+        isWearingAirPods = true
+
+        let attitude = motion.attitude
+        let pitch = attitude.pitch
+        let yaw = -attitude.yaw
+
+        filteredPitch = filteredPitch + filterAlpha * (pitch - filteredPitch)
+        filteredYaw = filteredYaw + filterAlpha * (yaw - filteredYaw)
+
+        let movementThreshold = 0.0088
+        let finalPitch = abs(filteredPitch) < movementThreshold ? 0 : filteredPitch
+        let finalYaw = abs(filteredYaw) < movementThreshold ? 0 : filteredYaw
+
+        let normalizePi: Double = .pi / cursorSensitivity
+        let normalizedPitch = ((finalPitch) + normalizePi / 2) / normalizePi + pitchOffset
+        let normalizedYaw = ((finalYaw) + normalizePi) / (2 * normalizePi) + yawOffset
+
+        let screenWidth = screenFrame.width
+        let screenHeight = screenFrame.height
+
+        let mappedX = screenWidth * CGFloat(normalizedYaw)
+        let mappedY = screenHeight * (1 - CGFloat(normalizedPitch))
+
+        pitchForScroll = mappedY
+
+        let truncatedPitch = Double(Int(normalizedPitch * 100)) / 100
+        let truncatedYaw = Double(Int(normalizedYaw * 100)) / 100
+
+        sendMotionData(pitch: truncatedPitch, yaw: truncatedYaw, name: airPodsName, macBattery: MacBattery, airpodLeftBattery: airPodsLeftBattery ?? 0, airpodRightBattery: airPodsRightBattery ?? 0)
+
+        targetCursorPos = CGPoint(x: mappedX, y: mappedY)
+    }
+
+    Timer.scheduledTimer(withTimeInterval: 1.0 / 60.0, repeats: true) { _ in
+        guard !isMotionPaused else { return }
+
+        func lerp(_ a: CGFloat, _ b: CGFloat, t: CGFloat) -> CGFloat {
+            return a + (b - a) * t
+        }
+
+        currentCursorPos.x = lerp(currentCursorPos.x, targetCursorPos.x, t: 0.2)
+        currentCursorPos.y = lerp(currentCursorPos.y, targetCursorPos.y, t: 0.2)
+        moveCursor(to: currentCursorPos)
+    }
+
+    Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
+        let timeSinceLastMotion = Date().timeIntervalSince(lastMotionTimestamp)
+        let maxInactiveDuration: TimeInterval = 2.0
+
+        if timeSinceLastMotion > maxInactiveDuration {
+            if isWearingAirPods {
+                isWearingAirPods = false
+                print("에어팟 감지 안됨. 프로그램 종료.")
+                exit(0)
+            }
+        }
+    }
+
 }
-
-Timer.scheduledTimer(withTimeInterval: 0.5, repeats: true) { _ in
-   let timeSinceLastMotion = Date().timeIntervalSince(lastMotionTimestamp)
-   let maxInactiveDuration: TimeInterval = 2.0
-
-   if timeSinceLastMotion > maxInactiveDuration {
-       if isWearingAirPods {
-           isWearingAirPods = false
-           print("에어팟 감지 안됨. 프로그램 종료.")
-           exit(0)
-       }
-   }
-}
-
-airPodsInfoTeller()
-Timer.scheduledTimer(withTimeInterval: 30.0, repeats: true) { _ in
-    airPodsInfoTeller()
-}
-
-RunLoop.main.run()
