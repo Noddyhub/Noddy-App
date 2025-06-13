c60f7ea9b78525eecce724370e96a3f298d1f356
박재연 <159511478+wodus1201@users.noreply.github.com>
2025-06-13 17:37:58 +0900

    refactor: 기술적 한계로 인한 사용불가 함수 삭제 #18
    * 앱 샌드박스 시 보안으로 인하여 해당 함수 실행 불가

--- swiftForNoddy/airPodsInfoTeller.swift
+++ swiftForNoddy/airPodsInfoTeller.swift
@@ -1,16 +1,4 @@
-import IOBluetooth
 import IOKit.ps
-
-func fetchPairedAirPodsName() -> String? {
-    if let devices = IOBluetoothDevice.pairedDevices() as? [IOBluetoothDevice] {
-        for device in devices {
-            if device.isConnected(), device.name.contains("AirPods") {
-                return device.name
-            }
-        }
-    }
-    return nil
-}
 
 func fetchMacBatteryLevel() -> Int? {
     let info = IOPSCopyPowerSourcesInfo().takeRetainedValue()
@@ -21,44 +9,4 @@ func fetchMacBatteryLevel() -> Int? {
         return nil
     }
     return level
-}
-
-func runBluetoothBatteryCommand() -> String? {
-    let command = "system_profiler SPBluetoothDataType | grep -i 'Battery Level'"
-    let process = Process()
-    let pipe = Pipe()
-    process.standardOutput = pipe
-    process.standardError = pipe
-    process.launchPath = "/bin/bash"
-    process.arguments = ["-c", command]
-    process.launch()
-    process.waitUntilExit()
-
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    return String(data: data, encoding: .utf8)
-}
-
-func parseAirPodsBatteryLevels(from output: String) {
-    output.enumerateLines { line, _ in
-        let cleanedLine = line.lowercased()
-        let value = Int(line.components(separatedBy: CharacterSet.decimalDigits.inverted).joined())
-
-        switch true {
-        case cleanedLine.contains("left"):
-            airPodsLeftBattery = value
-        case cleanedLine.contains("right"):
-            airPodsRightBattery = value
-        default:
-            break
-        }
-    }
-}
-
-func airPodsInfoTeller() {
-    airPodsName = fetchPairedAirPodsName() ?? "N/A"
-    MacBattery = fetchMacBatteryLevel() ?? 0
-
-    if let output = runBluetoothBatteryCommand() {
-        parseAirPodsBatteryLevels(from: output)
-    }
 }
